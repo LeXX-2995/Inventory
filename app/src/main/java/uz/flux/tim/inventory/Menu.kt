2a07e1321e901e9c366ec981962d1c3dfdd0fee8
Tim <tim@flux.uz>
2018-04-09 02:30:11 +0500

    added menu

---
+++ app/src/main/java/uz/flux/tim/inventory/Menu.kt
@@ -0,0 +1,12 @@
+package uz.flux.tim.inventory
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
---
+++ app/src/main/java/uz/flux/tim/inventory/Room_id.kt
@@ -0,0 +1,12 @@
+package uz.flux.tim.inventory
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Room_id : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_id)
+    }
+}
